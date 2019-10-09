7c1099dc613e07eecbc1fa61fb8b0fdefbbc498f
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2019-10-09 17:13:21 +0200

    juhu

--- busstationDB.sql
+++ busstationDB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Sep 15, 2019 at 01:58 PM
+-- Generation Time: Sep 17, 2019 at 06:05 PM
 -- Server version: 5.7.24
 -- PHP Version: 7.2.14
 
@@ -49,10 +49,10 @@ CREATE TABLE IF NOT EXISTS `bus` (
 --
 
 INSERT INTO `bus` (`ID`, `startDestination`, `endDestination`, `price`, `busType`, `company`, `route`, `maxSeat`, `availableSeat`, `departureTime`, `arrivalTime`) VALUES
-(1, 'Sarajevo', 'Mostar', 5, 'small', 'centrotrans', 'Zenica, Jajce', 30, 28, '17:00:00', '20:00:00'),
-(5, 'Dobrinja', 'Vijecnica', 5, 'middle', 'gras', 'Grbavica, Malta', 50, 49, '13:00:00', '13:45:00'),
-(6, 'Sarajevo', 'Bihac', 10, 'VIP', 'centrotrans', 'Jajce, Mostar', 45, 45, '15:00:00', '21:00:00'),
-(7, 'juhu', 'juhu', 10, 'juhu', 'juhu', 'juhu', 100, 100, '18:00:00', '19:45:00');
+(1, 'Sarajevo', 'Mostar', 5, 'small', 'centrotrans', 'Zenica, Jajce', 30, 19, '17:00:00', '20:00:00'),
+(5, 'Sarajevo', 'Zenica', 5, 'middle', 'gras', 'Grbavica, Malta', 50, 49, '13:00:00', '13:45:00'),
+(6, 'Sarajevo', 'Bihac', 10, 'VIP', 'centrotrans', 'Jajce, Mostar', 45, 44, '15:00:00', '21:00:00'),
+(7, 'Sarajevo', 'Konjic', 10, 'small', 'gras', 'Jajce', 100, 99, '18:00:00', '19:45:00');
 
 -- --------------------------------------------------------
 
@@ -79,6 +79,29 @@ INSERT INTO `register` (`ID`, `email`, `name`, `password`, `type`) VALUES
 (3, 'dzejlana.kovacevic@gmail.com', 'Dzejlana Kovacevic', '$2y$10$kjHhOkwdjX.GT95L.b3nwu/6vssI4wExjtai5je5ZCqkm51Vji5xC', 1),
 (2, 'abu@gmail.com', 'abu', '$2y$10$0crSDP6MZgkzKg7ajMSTQeFZJntpv1mVsZ/X/xMndnooyf5cPB59q', 0),
 (6, 'combe@hotmail.com', 'combe', '$2y$10$ehVZB02/vNSKZNDMYcOtKu6eRkNsAXOX/XH/T1okuFPTSNSjjM.Da', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reserved`
+--
+
+DROP TABLE IF EXISTS `reserved`;
+CREATE TABLE IF NOT EXISTS `reserved` (
+  `ID` int(100) NOT NULL AUTO_INCREMENT,
+  `user` varchar(250) NOT NULL,
+  `busID` int(100) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `reserved`
+--
+
+INSERT INTO `reserved` (`ID`, `user`, `busID`) VALUES
+(1, ' abu@gmail.com ', 1),
+(3, ' abu@gmail.com ', 7),
+(4, ' abu@gmail.com ', 6);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
